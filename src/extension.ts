9535a09eb88ccd591c1180278e572e7c99524ba5
Luca Engelhard <me@lucaengelhard.com>
2024-02-13 21:10:27 +0100

    get todos from docs in workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,42 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  console.log("notion-todo is now active!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "notion-todo" is now active!');
+  const currentDocLang = vscode.window.activeTextEditor?.document.languageId;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('notion-todo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Notion Todo!');
-	});
+  const workspaceFiles = await vscode.workspace.findFiles(
+    "**/*.{ts,js}",
+    "**/{node_modules,dist}/**"
+  );
 
-	context.subscriptions.push(disposable);
+  workspaceFiles.forEach(async (file) => {
+    await getToDos(file);
+  });
+
+  context.subscriptions.push();
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function getToDos(fileUri: vscode.Uri) {
+  //const file = await vscode.workspace.fs.readFile(fileUri);
+  const file = await vscode.workspace.openTextDocument(fileUri);
+
+  const regex = /\/\/\s*TODO:.*|\/\*\s*TODO:[\s\S]*?\*\//gm;
+
+  const toDos = file.getText().match(regex);
+
+  if (!toDos) {
+    return;
+  }
+
+  toDos.forEach((toDo) => {
+    console.log(toDo);
+  });
+}
